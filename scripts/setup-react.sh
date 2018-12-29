22bb0060ea8b3ec9dafddf82fe0eecce8dac5e59
ali eissa <aeissa.o@gmail.com>
2018-12-29 01:34:52 -0500

    Add weback to setup script

--- scripts/setup-react.sh
+++ scripts/setup-react.sh
@@ -1,24 +1,41 @@
 #!/bin/sh
 
-# echo "Adding React packes ----------------------"
+echo "Adding React packes ----------------------"
 yarn add react react-dom
 
-# echo "Adding Babel packages ----------------------"
+echo "Adding Babel packages ----------------------"
 yarn add @babel/core babel-loader @babel/preset-env @babel/preset-react --dev
 
-# echo "Adding CSS loaders ----------------------"
+echo "Adding CSS loaders ----------------------"
 yarn add css-loader style-loader --dev
+
+echo "Adding webpack packages ----------------------"
+yarn add webpack webpack-cli webpack-dev-server --dev
+
+# Create index.js file
+cat <<EOF > index.js
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+
+ReactDOM.render(<App />, document.getElementById('root'));
+EOF
 
 # Create .babelrc set presets
 cat <<EOF > webpack.config.js
 const path = require("path");
 
 module.exports = {
-  entry: "./src/index.js",
+  entry: path.join(__dirname, 'src', 'index.js'),
   output: {
     path: path.join(__dirname, "/dist"),
     filename: "index_bundle.js"
   },
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    historyApiFallback: true // Needed to make React router work without using # in URL
+  },
+  mode: "development",
   module: {
     rules: [
       {
