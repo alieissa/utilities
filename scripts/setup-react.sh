9bf58b392d7dfcfc537d43c071387d5708ae3f0c
ali eissa <aeissa.o@gmail.com>
2018-12-29 17:36:12 -0500

    Add missing testing setup files and update test package installation cmd

--- scripts/setup-react.sh
+++ scripts/setup-react.sh
@@ -13,7 +13,7 @@ echo "Adding webpack packages ----------------------"
 yarn add webpack webpack-cli webpack-dev-server html-webpack-plugin --dev
 
 echo "Adding testing packages ----------------------"
-yarn add --dev jest jest-cli enzyme
+yarn add --dev jest jest-cli enzyme enzyme-adapter-react-16 sinon
 # This is needed to make jest work with babel version 7
 yarn add --dev babel-jest babel-core@^7.0.0-bridge.0 @babel/core
 
@@ -26,6 +26,33 @@ import App from './App';
 ReactDOM.render(<App />, document.getElementById('app-container'));
 EOF
 
+# styleMocks determines how css modules are to be handled
+# in this case they are ignored
+mkdir __mocks__
+cat<<EOF > __mocks__/styleMock.js
+module.exports = {};
+EOF
+
+# Create jest.config.js. This is necessary to run tests
+# moduleNAmeMApper is needed in order disregard css modules
+# enzyme.setup.js is needed because we setup an adapter before running test
+cat <<EOF > jest.config.js
+const path = require("path")
+module.exports = {
+  verbose: true,
+  moduleNameMapper: {
+    ".css$": path.join(__dirname, "__mocks__/styleMock.js")  
+ },
+ setupTestFrameworkScriptFile: path.join(__dirname, "enzyme.setup.js")
+};
+EOF
+
+# Create enzyme config file
+cat <<EOF >enzyme.setup.js
+import Enzyme, { mount, render, shallow } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+Enzyme.configure({ adapter: new Adapter() });
+EOF
 # Create .babelrc set presets
 cat <<EOF > webpack.config.js
 const HtmlWebpackPlugin = require("html-webpack-plugin");
